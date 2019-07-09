59b1f52eae2296fffafa2ded178ef8d1b147cde0
ozgurbariskas <43556376+ozgurbariskas@users.noreply.github.com>
2019-07-09 13:14:54 +0300

    Add files via upload
    İnitialize

---
+++ GalleryApplication/wwwroot/js/script.js
@@ -0,0 +1,15 @@
+﻿function loadItems() {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+        var myObj = JSON.parse(this.responseText);
+        for (x in myObj.Employees) {
+            document.getElementById("item-container").innerHTML += "<tr><td>" + myObj.Employees[x]["firstName"] + "</td><td>" + myObj.Employees[x]["lastName"]
+            +"</td><td>" + myObj.Employees[x]["jobTitle"] + "</td></tr>";
+        }
+    }
+  };
+  xmlhttp.open("GET", "items.json", true);
+    xmlhttp.send();
+    document.getElementById("loadbutton").style.display="none";
+}
